7ff1fd03a001a75c092f49569ae54fd2fa93abb6
Heiko W. Rupp <hwr@pilhuhn.de>
2015-01-31 22:01:13 +0100

    HWKINVENT-1 Provide a basic inventory implementation.

---
+++ impl/src/main/java/org/hawkular/inventory/impl/InventoryService.java
@@ -0,0 +1,268 @@
+/*
+ * Copyright 2015 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.hawkular.inventory.impl;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.hawkular.inventory.api.Inventory;
+import org.hawkular.inventory.api.MetricDefinition;
+import org.hawkular.inventory.api.Resource;
+import org.hawkular.inventory.api.ResourceType;
+import org.hawkular.inventory.impl.db.DbManager;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.ejb.Stateless;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+
+/**
+ * The inventory backend. Currently using the WildFly embedded H2
+ * @author Heiko Rupp
+ */
+@Stateless
+public class InventoryService implements Inventory {
+
+    @javax.annotation.Resource( lookup = "java:/jdbc/HawkularDS")
+    private DataSource db;
+
+    Gson gson;
+    PreparedStatement insertResourceStatement;
+    PreparedStatement findResourceByTypeStatement;
+    PreparedStatement findResourceByIdStatement;
+    PreparedStatement deleteResourceByIdStatement;
+    private PreparedStatement addMetricToResourceStatement;
+    private PreparedStatement listMetricsOfResourceStatement;
+    Connection connection;
+    private PreparedStatement deleteMetricsOfResourceStatement;
+
+    public InventoryService() {
+
+        gson = new GsonBuilder().create();
+
+    }
+
+    public InventoryService(Connection conn) {
+        this();
+        try {
+            DbManager.setupDB(conn);
+            prepareH2Statements(conn);
+        } catch (Exception e) {
+            e.printStackTrace();  // TODO: Customise this generated block
+        }
+    }
+
+    @PostConstruct
+    public void startup() {
+
+        if (db == null) {
+            Log.LOG.warn("Backend database not available");
+            throw new RuntimeException("No db, can't continue");
+        }
+
+        try {
+            connection = db.getConnection("sa","sa");
+            DbManager.setupDB(connection);
+            prepareH2Statements(connection);
+        } catch (Exception e) {
+            e.printStackTrace();  // TODO: Customise this generated block
+        }
+
+    }
+
+    @PreDestroy
+    public void cleanup() {
+        if (connection!=null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();  // TODO: Customise this generated block
+            }
+        }
+    }
+
+    @Override
+    public String addResource(String tenant, Resource resource) throws Exception {
+
+        String id = resource.getId();
+        if (id == null || id.isEmpty()) {
+            id = createUUID();
+            resource.setId(id);
+        }
+
+        insertResourceStatement.setString(1, id);
+        insertResourceStatement.setString(2, tenant);
+        insertResourceStatement.setString(3, resource.getType().name());
+        String payload = toJson(resource);
+        insertResourceStatement.setString(4, payload);
+        insertResourceStatement.execute();
+
+        return id;
+    }
+
+    @Override
+    public List<Resource> getResourcesForType(String tenant, ResourceType type) throws Exception {
+
+        List<Resource> result = new ArrayList<>();
+
+        findResourceByTypeStatement.setString(1, type.name());
+        findResourceByTypeStatement.setString(2, tenant);
+        ResultSet resultSet = findResourceByTypeStatement.executeQuery();
+        while (resultSet.next()) {
+            String payload = resultSet.getString(1);
+            Resource resource = fromJson(payload, Resource.class);
+            result.add(resource);
+        }
+        resultSet.close();
+
+        return result;
+    }
+
+    @Override
+    public Resource getResource(String tenant, String uid) throws Exception {
+
+        Resource result = null;
+
+        findResourceByIdStatement.setString(1, uid);
+        findResourceByIdStatement.setString(2, tenant);
+
+        ResultSet resultSet = findResourceByIdStatement.executeQuery();
+        while (resultSet.next()) {
+            String payload = resultSet.getString(1);
+            result = fromJson(payload, Resource.class);
+        }
+        resultSet.close();
+
+        return result;
+    }
+
+    @Override
+    public boolean deleteResource(String tenant, String uid) throws Exception {
+
+        deleteMetricsOfResourceStatement.setString(1, uid);
+        deleteMetricsOfResourceStatement.setString(2, tenant);
+        deleteMetricsOfResourceStatement.executeUpdate();
+
+        deleteResourceByIdStatement.setString(1, uid);
+        deleteResourceByIdStatement.setString(2, tenant);
+        int count = deleteResourceByIdStatement.executeUpdate();
+
+        return count ==1;
+    }
+
+    @Override
+    public boolean addMetricToResource(String tenant, String resourceId, String metric_name) throws Exception {
+        List<MetricDefinition> definitions = new ArrayList<>(1);
+        definitions.add(new MetricDefinition(metric_name));
+        return addMetricsToResource(tenant,resourceId,definitions);
+    }
+
+    @Override
+    public boolean addMetricsToResource(String tenant, String resourceId, Collection<MetricDefinition> definitions)
+            throws Exception {
+
+        try {
+
+            for (MetricDefinition definition : definitions) {
+                addMetricToResourceStatement.setString(1, resourceId);
+                addMetricToResourceStatement.setString(2, tenant);
+                addMetricToResourceStatement.setString(3, definition.getName());
+                addMetricToResourceStatement.setString(4, toJson(definition));
+
+                addMetricToResourceStatement.addBatch();
+
+            }
+            addMetricToResourceStatement.executeBatch();
+        } catch (SQLException e) {
+            if (!e.getSQLState().equals("23505")) { // violated PK - we don't care
+                Log.LOG.warn(e.getMessage());
+            }
+            return false;
+        }
+
+        return true;
+    }
+
+
+    @Override
+    public List<MetricDefinition> listMetricsForResource(String tenant, String resourceId) throws Exception {
+
+        List<MetricDefinition> result = new ArrayList<>();
+
+        listMetricsOfResourceStatement.setString(1,resourceId);
+        listMetricsOfResourceStatement.setString(2, tenant);
+
+        ResultSet resultSet = listMetricsOfResourceStatement.executeQuery();
+        while (resultSet.next()) {
+            String payload = resultSet.getString(1);
+
+            result.add(fromJson(payload,MetricDefinition.class));
+        }
+        resultSet.close();
+
+        return result;
+    }
+
+    private String createUUID() {
+        return "x" + String.valueOf(System.currentTimeMillis());
+    }
+
+
+
+    void prepareH2Statements(Connection c ) throws Exception {
+
+        // deal with resources
+        insertResourceStatement = c.prepareStatement("INSERT INTO HWK_RESOURCES VALUES ( ?, ?, ?, ? ) ");
+        findResourceByIdStatement =
+                c.prepareStatement("SELECT r.payload FROM HWK_RESOURCES r  WHERE ID = ? AND TENANT = ?");
+        findResourceByTypeStatement =
+                c.prepareStatement("SELECT r.payload FROM HWK_RESOURCES r WHERE type = ? AND tenant = ?");
+        deleteResourceByIdStatement = c.prepareStatement("DELETE FROM HWK_RESOURCES WHERE ID = ? AND TENANT = ?");
+
+        // deal with metrics
+        addMetricToResourceStatement = c.prepareStatement("INSERT INTO HWK_METRICS VALUES ( ?,?,?, ?)");
+        listMetricsOfResourceStatement = c.prepareStatement("SELECT m.payload FROM HWK_METRICS m WHERE m" +
+                ".resource_id = ? AND TENANT = ?");
+
+        deleteMetricsOfResourceStatement =
+                c.prepareStatement("DELETE FROM HWK_METRICS where resource_id = ? AND TENANT = ?");
+
+    }
+
+    private String toJson(Object resource) {
+
+        return gson.toJson(resource);
+
+    }
+
+    private <T> T fromJson(String json, Class<T> clazz) {
+
+        return gson.fromJson(json,clazz);
+    }
+
+    @SuppressWarnings("unused")
+    public void setConnection(Connection conn) {
+        // used for mocking
+    }
+}
---
+++ rest-servlet/src/main/java/org/hawkular/inventory/rest/RestApi.java
@@ -0,0 +1,185 @@
+/*
+ * Copyright 2015 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.hawkular.inventory.rest;
+
+
+import org.hawkular.inventory.api.Inventory;
+import org.hawkular.inventory.api.MetricDefinition;
+import org.hawkular.inventory.api.Resource;
+import org.hawkular.inventory.api.ResourceType;
+
+import javax.ejb.EJB;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+import java.util.Collection;
+
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+
+/**
+ * The Rest api for Hawkular Inventory
+ * @author Heiko Rupp
+ */
+@Path("/")
+@Produces(value = APPLICATION_JSON)
+@Consumes(value = APPLICATION_JSON)
+public class RestApi {
+
+
+    @EJB
+    Inventory inventory;
+
+
+    @GET
+    @Path("/")
+    public StringWrapper ping() {
+        return new StringWrapper("Hello World");
+    }
+
+    @POST
+    @Path("/{tenantId}/resources")
+    public Response addResource(@PathParam("tenantId") String tenantId,
+                            Resource definition) {
+
+        try {
+            String id = inventory.addResource(tenantId, definition);
+
+            return Response.ok(new IdWrapper(id)).build();
+        } catch (Exception e) {
+            RestApiLogger.LOGGER.warn(e);
+            return Response.serverError().entity(e).build();
+        }
+    }
+
+
+    @GET
+    @Path("/{tenantId}/resources")
+    public Response getResourcesByType(@PathParam("tenantId") String tenantId,
+                                       @QueryParam("type") String type) {
+
+        ResourceType rtype = ResourceType.valueOf(type.toUpperCase());
+
+        try {
+            Collection<Resource> resources = inventory.getResourcesForType(tenantId, rtype);
+            return Response.ok(resources).build();
+        } catch (Exception e) {
+            RestApiLogger.LOGGER.warn(e);
+            return Response.serverError().entity(e).build();
+        }
+    }
+
+    @GET
+    @Path("/{tenantId}/resource/{uid}")
+    public Response getResource(@PathParam("tenantId") String tenantId, @PathParam
+            ("uid") String uid) {
+
+        try {
+            Resource def = inventory.getResource(tenantId, uid);
+
+            if (def != null) {
+                return Response.ok(def).build();
+            } else {
+                return Response.status(Response.Status.NOT_FOUND).build();
+            }
+
+        } catch (Exception e) {
+            RestApiLogger.LOGGER.warn(e);
+            return Response.serverError().build();
+        }
+    }
+
+
+    @DELETE
+    @Path("/{tenantId}/resource/{uid}")
+    public Response deleteResource(@PathParam("tenantId") String tenantId, @PathParam
+            ("uid") String uid) {
+
+        try {
+            boolean def = inventory.deleteResource(tenantId, uid);
+
+            if (def) {
+                return Response.ok(def).build();
+            } else {
+                return Response.status(Response.Status.NOT_FOUND).build();
+            }
+
+        } catch (Exception e) {
+            RestApiLogger.LOGGER.warn(e);
+            return Response.serverError().build();
+        }
+    }
+
+
+    @PUT
+    @Path("/{tenantId}/resource/{resourceId}/metrics/")
+    public Response addMetricToResource(@PathParam("tenantId") String tenantId,
+                                        @PathParam("resourceId") String resourceId,
+                                        Collection<MetricDefinition> payload) {
+
+
+        try {
+
+            if (inventory.getResource(tenantId, resourceId)==null) {
+                return Response.status(404).entity("Resource with ID " + resourceId + " not found for tenant").build();
+            }
+
+            if (payload.size() == 0) {
+                return Response.status(Response.Status.BAD_REQUEST).build();
+            }
+
+            boolean def = inventory.addMetricsToResource(tenantId, resourceId, payload);
+
+            if (def) {
+                return Response.ok(def).build();
+            } else {
+                return Response.status(Response.Status.NOT_MODIFIED).build();
+            }
+
+        } catch (Exception e) {
+            RestApiLogger.LOGGER.warn(e);
+            return Response.serverError().build();
+        }
+    }
+
+    @GET
+    @Path("/{tenantId}/resource/{resourceId}/metrics")
+    public Response listMetricsOfResource(@PathParam("tenantId") String tenantId,
+                                            @PathParam("resourceId") String resourceId) {
+
+
+        try {
+
+            if (inventory.getResource(tenantId, resourceId)==null) {
+                return Response.status(404).entity("Resource with ID " + resourceId + " not found for tenant").build();
+            }
+
+            Collection<MetricDefinition> bla = inventory.listMetricsForResource(tenantId, resourceId);
+            return Response.ok(bla).build();
+        } catch (Exception e) {
+            RestApiLogger.LOGGER.warn(e);
+            return Response.serverError().entity(e).build();
+        }
+
+    }
+}
