d69e81e86f3cb070d066a33195e084c6d91e1ac9
Lukas Krejci <lkrejci@redhat.com>
2015-09-30 12:37:22 +0200

    [HWKINVENT-65] Automatically associate feeds with the test env in TemporaryHacks.
    This is so that people don't have to do that manually until we have
    support for environments in the UI.

--- hawkular-inventory-dist/src/main/java/org/hawkular/integrated/inventory/TemporaryHacks.java
+++ hawkular-inventory-dist/src/main/java/org/hawkular/integrated/inventory/TemporaryHacks.java
@@ -18,13 +18,16 @@ package org.hawkular.integrated.inventory;
 
 import static org.hawkular.inventory.api.Action.created;
 
+import java.util.Set;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
 
+import org.hawkular.inventory.api.Environments;
 import org.hawkular.inventory.api.Interest;
 import org.hawkular.inventory.api.Inventory;
-import org.hawkular.inventory.api.PartiallyApplied;
 import org.hawkular.inventory.api.model.Environment;
+import org.hawkular.inventory.api.model.Feed;
 import org.hawkular.inventory.api.model.MetricDataType;
 import org.hawkular.inventory.api.model.MetricType;
 import org.hawkular.inventory.api.model.MetricUnit;
@@ -45,15 +48,30 @@ public class TemporaryHacks {
         Inventory inventory = event.getInventory();
 
         Observable<Tenant> tenantCreation = inventory.observable(Interest.in(Tenant.class).being(created()));
-        tenantCreation.subscribe(PartiallyApplied.procedure(this::createTenantMetadata).second(inventory),
+        tenantCreation.subscribe((t) -> createTenantMetadata(t, inventory),
                 Log.LOGGER::failedToAutoCreateEntities);
-        tenantCreation.subscribe(PartiallyApplied.procedure(this::createTestEnvironment).second(inventory),
+        tenantCreation.subscribe((t) -> createTestEnvironment(t, inventory),
+                Log.LOGGER::failedToAutoCreateEntities);
+
+        Observable<Feed> feedCreation = inventory.observable(Interest.in(Feed.class).being(created()));
+        feedCreation.subscribe((f) -> associateWithTestEnvironment(f, inventory),
                 Log.LOGGER::failedToAutoCreateEntities);
     }
 
     private void createTestEnvironment(Tenant tenant, Inventory inventory) {
-        inventory.inspect(tenant).environments().create(Environment.Blueprint.builder().withId("test").build());
+        Environments.Single env = inventory.inspect(tenant).environments().create(Environment.Blueprint.builder().withId
+                ("test").build());
         Log.LOGGER.autoCreatedEntity("environment", "test", tenant.getId());
+
+        Set<Feed> allFeeds = inventory.inspect(tenant).feeds().getAll().entities();
+        Set<Feed> associatedFeeds = env.feeds().getAll().entities();
+
+        allFeeds.forEach((feed) -> {
+            if (!associatedFeeds.contains(feed)) {
+                env.feeds().associate(feed.getPath());
+                Log.LOGGER.autoAssociatedFeed(feed.getPath().toString());
+            }
+        });
     }
 
     private void createTenantMetadata(Tenant tenant, Inventory inventory) {
@@ -68,8 +86,17 @@ public class TemporaryHacks {
 
         inventory.inspect(tenant).feedlessMetricTypes()
                 .create(MetricType.Blueprint.builder(MetricDataType.GAUGE).withId("status.duration.type")
-                .withUnit(MetricUnit.MILLISECONDS).build());
+                        .withUnit(MetricUnit.MILLISECONDS).build());
         Log.LOGGER.autoCreatedEntity("metric type", "status.duration.type", tenant.getId());
     }
 
+    private void associateWithTestEnvironment(Feed feed, Inventory inventory) {
+        Environments.Single env = inventory.tenants().get(feed.getPath().ids().getTenantId()).environments()
+                .get("test");
+
+        if (env.exists()) {
+            env.feeds().associate(feed.getPath());
+            Log.LOGGER.autoAssociatedFeed(feed.getPath().toString());
+        }
+    }
 }
