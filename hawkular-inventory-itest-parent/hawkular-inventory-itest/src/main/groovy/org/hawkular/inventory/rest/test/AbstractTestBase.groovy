1f979d0c3d0b2959a9802bcde4d62463b781d713
Lukas Krejci <lkrejci@redhat.com>
2016-01-12 13:39:18 +0100

    Update license years.

--- hawkular-inventory-itest-parent/hawkular-inventory-itest/src/main/groovy/org/hawkular/inventory/rest/test/AbstractTestBase.groovy
+++ hawkular-inventory-itest-parent/hawkular-inventory-itest/src/main/groovy/org/hawkular/inventory/rest/test/AbstractTestBase.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 Red Hat, Inc. and/or its affiliates
+ * Copyright 2015-2016 Red Hat, Inc. and/or its affiliates
  * and other contributors as indicated by the @author tags.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -18,7 +18,6 @@ package org.hawkular.inventory.rest.test
 
 import groovyx.net.http.ContentType
 import groovyx.net.http.RESTClient
-
 import org.junit.BeforeClass
 
 class AbstractTestBase {
